918d85bfe48b8918b603c61dc611d3a3e87b945c
Jujua_c <juliann.merit@gmail.com>
2023-01-18 09:38:38 +0100

    Update IA

--- client_joueur.py
+++ client_joueur.py
@@ -6,118 +6,9 @@ import const
 import plateau
 import case
 import joueur
+import fonctions_IA as IA
 
 def mon_IA(ma_couleur,carac_jeu, plan, les_joueurs):
-    def mur_autour_joueur(plan, ma_couleur, les_joueurs):
-        """ Cette fonction permet de savoir dans quelle direction le joueurs peut aller en fonction des murs
-        Args:
-            plan (str): le plan du plateau comme comme indiqué dans le sujet
-        Returns:
-            list: une liste de direction où le joueur peut aller
-        """
-        liste_joueurs = joueur.joueur_from_str(les_joueurs)
-        print(liste_joueurs)
-        liste_couleur = []
-        for i in liste_joueurs.keys():
-            if i == ["couleur"]:
-                liste_couleur.append(i.lower)
-        liste_couleur.append("#")
-        
-        pos = joueur.get_pos(ma_couleur)
-        direction_possible = [plan[pos[0]-1][pos[1]], plan[pos[0]+1][pos[1]], plan[pos[0]][pos[1]+1], plan[pos[0]][pos[1]-1]]
-        for direction in ["N","S","E","O"]:
-            if direction == "N":
-                if plan[pos[0]-1][pos[1]] in liste_couleur:
-                    direction_possible.remove(direction)
-            elif direction == "S":
-                if plan[pos[0]+1][pos[1]] in liste_couleur:
-                    direction_possible.remove(direction)
-            elif direction == "E":
-                if plan[pos[0]][pos[1]+1] in liste_couleur:
-                    direction_possible.remove(direction)
-            elif direction == "O":
-                if plan[pos[0]][pos[1]-1] in liste_couleur:
-                    direction_possible.remove(direction)
-        return direction_possible
-    
-
-    def case_vide_direction(plan,pos,direction):
-        """ Cette fonction permet d'obtenir la liste d'index des positions des cases vides)
-        Args:
-            plan (str): le plan du plateau comme indiqué dans le sujet
-            pos (tuple): la position du joueur
-            direction (str): la direction où on veut regarder
-        Returns:
-            list: une liste d'index des cases étant incolore
-        """
-        distance_max = 5
-        case_vides = []
-        pos2 = pos
-        INC_DIRECTION = {'N': (-1, 0), 'E': (0, 1), 'S': (1, 0), 'O': (0, -1), 'X': (0, 0)}
-        for index in range(distance_max):
-            if plateau.est_sur_plateau(plan,pos2) and not plan["cases"][pos2]["mur"]:
-                if plan["cases"][pos2]['couleur'] == ' ':
-                    case_vides.append(index+1)
-            pos2 = (pos2[0] + INC_DIRECTION[direction][0], pos2[1] + INC_DIRECTION[direction][1])
-        return case_vides
-
-    def case_peinte_autre_joueurs_direction(plan,pos,direction):
-        """ Cette fonction permet d'obtenir la liste d'index des positions des cases peintes par des autres joueur)
-        Args:
-            plan (str): le plan du plateau comme indiqué dans le sujet
-            pos (tuple): la position du joueur
-            direction (str): la direction où on veut regarder
-        Returns:
-            list: une liste d'index des cases étant peintes par des joueurs autres que nous même
-        """
-        distance_max = 5
-        case_joueurs = []
-        pos2 = pos
-        INC_DIRECTION = {'N': (-1, 0), 'E': (0, 1), 'S': (1, 0), 'O': (0, -1), 'X': (0, 0)}
-        for index in range(distance_max):
-            if plateau.est_sur_plateau(plan,pos2) and not plan["cases"][pos2]["mur"]:
-                if plan["cases"][pos2]['couleur'] != ' ':
-                    if plan["cases"][pos2]['couleur'] != ma_couleur:
-                        case_joueurs.append(index+1)
-            pos2 = (pos2[0] + INC_DIRECTION[direction][0], pos2[1] + INC_DIRECTION[direction][1])
-        return case_joueurs
-
-    def nb_cases_possibles_a_peindre_direction(plan,pos,reserve,direction):
-        """ Cette fonction permet d'obtenir le nombre de cases possibles à peindre dans une direction et un nombre de reserve donné
-        Args:
-            plan (str): le plan du plateau comme indiqué dans le sujet
-            pos (tuple): la position du joueur
-            reserve (int): le nombre de reserve du joueur
-            direction (str): la direction où on veut regarder
-        Returns:
-            int: le nombre de cases possibles à peindre dans une direction et un nombre de reserve donné
-        """
-        nombre_case = 0
-        case_vide = case_peinte_autre_joueurs_direction(plan,pos,direction)
-        case_peinte_autre_joueurs = case_peinte_autre_joueurs_direction(plan,pos,direction)
-        for i in range(1,6):
-            if i in case_vide:
-                if reserve >= 1:
-                    reserve -= 1
-                    nombre_case = 1+i
-            elif i in case_peinte_autre_joueurs:
-                if reserve >= 2:
-                    reserve -= 2
-                    nombre_case = 1+i
-        return nombre_case
-
-    def dico_reserve(plan,les_joueurs):
-        pass
-
-    def joueurs_plus_proche():
-        pass
-
-    def direction():
-        pass
-
-    def get_coords_objets():
-        pass
-
     """ Cette fonction permet de calculer les deux actions du joueur de couleur ma_couleur
         en fonction de l'état du jeu décrit par les paramètres. 
         Le premier caractère est parmi XSNOE X indique pas de peinture et les autres
@@ -138,15 +29,18 @@ def mon_IA(ma_couleur,carac_jeu, plan, les_joueurs):
             et la direction de déplacement
     """
     # IA complètement légitime et très forte (et extrêmement recherchée)
+    le_plan = plateau.Plateau(plan)
+    donnees_joueurs = joueur.joueur_from_str(les_joueurs)
     direction_mouvement = str(random.choice("NSEO"))
-    direction_mouvement = str(random.choice(mur_autour_joueur(plan,ma_couleur,les_joueurs)))
-    best_nb_case = 0
-    best_direction = 'X'
-    for direction_test in "NSEO":
-        if nb_cases_possibles_a_peindre_direction(plan,joueur.get_pos(ma_couleur),joueur.get_reserve(ma_couleur),direction_test) > best_nb_case:
-            best_nb_case = nb_cases_possibles_a_peindre_direction(plan,joueur.get_pos(ma_couleur),joueur.get_reserve(ma_couleur),direction_test)
-            best_direction = direction_test
-    return best_direction + direction_mouvement
+    # direction_mouvement = str(random.choice(mur_autour_joueur(plan,ma_couleur,les_joueurs)))
+    # best_nb_case = 0
+    # best_direction = 'X'
+    # for direction_test in "NSEO":
+    #     if nb_cases_possibles_a_peindre_direction(plan,joueur.get_pos(ma_couleur),joueur.get_reserve(ma_couleur),direction_test) > best_nb_case:
+    #         best_nb_case = nb_cases_possibles_a_peindre_direction(plan,joueur.get_pos(ma_couleur),joueur.get_reserve(ma_couleur),direction_test)
+    #         best_direction = direction_test
+
+    return 'X' + direction_mouvement
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()  
