c3f27a83b3a0d50c0419d3995b51b39318a1115e
DorianLudm <dorianludm7@gmail.com>
2023-01-19 22:59:39 +0100

    Last commit
    Gg wp, could havste been better but I enjoyed my stay

--- fonction_IA.py
+++ fonction_IA.py
@@ -7,38 +7,6 @@ import plateau
 import case
 import joueur
 
-
-def mur_autour_joueur(plan, ma_couleur, les_joueurs):
-    """ Cette fonction permet de savoir dans quelle direction le joueurs peut aller en fonction des murs
-    Args:
-        plan (str): le plan du plateau comme comme indiqué dans le sujet
-    Returns:
-        list: une liste de direction où le joueur peut aller
-    """
-    liste_couleur = []
-    for i in les_joueurs.keys():
-        if i == ["couleur"]:
-            liste_couleur.append(i.lower)
-    liste_couleur.append("#")
-    
-    pos = joueur.get_pos(ma_couleur)
-    direction_possible = [plan[pos[0]-1][pos[1]], plan[pos[0]+1][pos[1]], plan[pos[0]][pos[1]+1], plan[pos[0]][pos[1]-1]]
-    for direction in ["N","S","E","O"]:
-        if direction == "N":
-            if plan[pos[0]-1][pos[1]] in liste_couleur:
-                direction_possible.remove(direction)
-        elif direction == "S":
-            if plan[pos[0]+1][pos[1]] in liste_couleur:
-                direction_possible.remove(direction)
-        elif direction == "E":
-            if plan[pos[0]][pos[1]+1] in liste_couleur:
-                direction_possible.remove(direction)
-        elif direction == "O":
-            if plan[pos[0]][pos[1]-1] in liste_couleur:
-                direction_possible.remove(direction)
-    return direction_possible
-
-
 def case_vide_direction(plan,pos,direction):
     """ Cette fonction permet d'obtenir la liste d'index des positions des cases vides
     Args:
@@ -59,7 +27,7 @@ def case_vide_direction(plan,pos,direction):
         pos2 = (pos2[0] + INC_DIRECTION[direction][0], pos2[1] + INC_DIRECTION[direction][1])
     return case_vides
 
-def case_peinte_autre_joueurs_direction(plan,pos,direction,ma_couleur):
+def case_peinte_autre_joueurs_direction(plan,pos,direction,couleur):
     """ Cette fonction permet d'obtenir la liste d'index des positions des cases peintes par des autres joueur)
     Args:n
         plan (str): le plan du plateau comme indiqué dans le sujet
@@ -74,7 +42,7 @@ def case_peinte_autre_joueurs_direction(plan,pos,direction,ma_couleur):
     INC_DIRECTION = {'N': (-1, 0), 'E': (0, 1), 'S': (1, 0), 'O': (0, -1), 'X': (0, 0)}
     for index in range(distance_max):
         if plateau.est_sur_plateau(plan,pos2) and not plan["cases"][pos2]["mur"]:
-            if plan["cases"][pos2]['couleur'] != ' ':
+            if plan["cases"][pos2]['couleur'] != ' ' or plan["cases"][pos2]['couleur'] != couleur:
                 case_joueurs.append(index+1)
         pos2 = (pos2[0] + INC_DIRECTION[direction][0], pos2[1] + INC_DIRECTION[direction][1])
     return case_joueurs
